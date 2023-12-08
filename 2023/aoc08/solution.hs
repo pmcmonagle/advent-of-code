88d41fb3a4d583e98477f4bf3944c962e3b4f7f7
Paul McMonagle <mcmonagle.paul@gmail.com>
2023-12-08 20:04:17 +0000

    not quite day 8 part 2

--- 2023/aoc08/solution.hs
+++ 2023/aoc08/solution.hs
@@ -20,6 +20,24 @@ solution_a input =
         mp = Map.fromList $ map parseMapEntry (drop 2 ls)
     in navigate mp "AAA" is
 
+-- Solution B requires navigating a list of locations
+-- This takes *forever* and I bet I need to find a fixed point for each start location
+-- and calculate when they all overlap
+navigate' mp locs [] = []
+navigate' mp locs (p:ps)
+    | all ((=='Z') . head) locs = []
+    | otherwise                 = p:(navigate' mp nexts ps)
+    where nexts = if p == 'L' then map (fst . (mp Map.!)) locs else map (snd . (mp Map.!)) locs
+
+solution_b input =
+    let ls = lines input
+        is = cycle $ head ls
+        xs = map parseMapEntry (drop 2 ls)
+        ss = filter ((=='A') . last) (map fst xs)
+        mp = Map.fromList xs
+    in navigate' mp ss is
+
 main = do
     input <- getContents
     print . length $ solution_a input
+    print . length $ solution_b input
